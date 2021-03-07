5d4eebdf8f3cef1b2feb5246c94dabc5b7ab3fdb
SC-47 <max1839757@gmail.com>
2021-03-07 20:09:49 +0800

    0307: commit test

--- Mar/Mar_week2/file manager system/src/Main.java
+++ Mar/Mar_week2/file manager system/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String a = "hello2";
+        String a = "hello1";
         final String b = "hello";
         String d = "hello";
-        String c = b + 2;
-        String e = d + 2;
+        String c = b + 1;
+        String e = d + 1;
         System.out.println((a == c));
         System.out.println((a == e));
     }
